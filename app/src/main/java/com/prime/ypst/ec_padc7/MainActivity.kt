8c012273ab7201a2ec5e4a858e96ee1774aa3394
Ye Pyae Sone Tun <yepyaesone.tun@mm-digital-solutions.com>
2019-04-24 22:42:54 +0630

    Initial Commit

---
+++ app/src/main/java/com/prime/ypst/ec_padc7/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.prime.ypst.ec_padc7
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
